3725a64bd8d1646b2b8c30bd317e910dfeede9cc
Bhuvanesh Prasad <pbhuvanesh3@gmail.com>
2024-07-20 14:07:59 +0530

    added model training pipeline to main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from instanceSegmentation import logger
 from instanceSegmentation.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 from instanceSegmentation.pipeline.stage_02_model_building import ModelBuildingPipeline
+from src.instanceSegmentation.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 load_dotenv()
 
@@ -21,6 +22,17 @@ STAGE_NAME = "Model Building Stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     obj = ModelBuildingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = " Model Training Pipeline"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = ModelTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<")
 except Exception as e:
